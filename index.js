00701a1338260b23e4af46d764656bddd482faa2
Kwabs HP <khobewayne@gmail.com>
2024-02-12 17:58:28 +0000

    session middleware configured

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import bcrypt from "bcrypt";
+import session from "express-session";
 
 const app = express();
 const port = 3000;
@@ -20,6 +21,13 @@ const saltRounds = 10;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.use(session({
+  secret:"TOPSECRET",
+  resave: false,
+  saveUninitialized: true
+}))
+
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
